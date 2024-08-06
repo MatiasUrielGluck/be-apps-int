b9efcfec0f5a13bfea7290cb62ddad9297ae843f
matiasgluck <matiasugluck@gmail.com>
2024-08-06 19:33:25 -0300

    feat: git init

---
+++ src/main/java/com/uade/beappsint/BeAppsIntApplication.java
@@ -0,0 +1,13 @@
+package com.uade.beappsint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAppsIntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAppsIntApplication.class, args);
+    }
+
+}
