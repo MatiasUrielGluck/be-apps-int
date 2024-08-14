9378a7b8aae20f932d1a942ed66c490a97b6be6b
TobaMedinaMELI <tobias.medinaviegas@mercadolibre.com>
2024-08-13 21:13:37 -0300

    @tag and @operation

--- src/main/java/com/uade/beappsint/controller/AuthController.java
+++ src/main/java/com/uade/beappsint/controller/AuthController.java
@@ -5,8 +5,11 @@ import com.uade.beappsint.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Authentication", description = "Endpoints for user authentication")
 @RequestMapping("/auth")
 @RestController
 public class AuthController {
@@ -16,6 +19,7 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Sign up a new user", description = "Creates a new user in the system")
     @PostMapping("/signup")
     public ResponseEntity<SignupResponseDTO> signup(@RequestBody @Validated SignupRequestDTO request) {
         return ResponseEntity
@@ -23,6 +27,7 @@ public class AuthController {
                 .body(authService.signup(request));
     }
 
+    @Operation(summary = "Log in an existing user", description = "Authenticates a user and returns a token")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Validated LoginRequestDTO loginRequestDTO) {
         return ResponseEntity
@@ -30,6 +35,7 @@ public class AuthController {
                 .body(authService.login(loginRequestDTO));
     }
 
+    @Operation(summary = "Get customer information", description = "Retrieves information about the authenticated customer")
     @GetMapping("/info")
     public ResponseEntity<CustomerInfoDTO> customerInfo() {
         return ResponseEntity
@@ -37,6 +43,7 @@ public class AuthController {
                 .body(authService.getCustomerInfo());
     }
 
+    @Operation(summary = "Test public endpoint", description = "A public endpoint for testing purposes")
     @PostMapping("/test")
     public String publicEndpointTest() {
         return "Hello World from UADE";
