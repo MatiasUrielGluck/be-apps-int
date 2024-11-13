d504b30500b77dc107aa1c3367db40e431b0fd78
matiasgluck <matiasugluck@gmail.com>
2024-11-13 18:49:50 -0300

    feat: cloudinary integration

--- src/main/java/com/uade/beappsint/service/impl/CloudinaryServiceImpl.java
+++ src/main/java/com/uade/beappsint/service/impl/CloudinaryServiceImpl.java
@@ -2,7 +2,9 @@ package com.uade.beappsint.service.impl;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import com.uade.beappsint.exception.BadRequestException;
 import com.uade.beappsint.service.CloudinaryService;
+import com.uade.beappsint.utils.CommonUtilities;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ public class CloudinaryServiceImpl implements CloudinaryService {
 
     @Override
     public String uploadImageBase64(String base64) {
+        String imageFormat = CommonUtilities.extractImageFormat(base64);
+        if (!CommonUtilities.isValidImageFormat(imageFormat)) throw new BadRequestException("File format is not supported");
+
         try {
             var pic = cloudinary.uploader().upload(base64, ObjectUtils.asMap("folder", "/blacknuster/"));
             return pic.get("url").toString();
--- src/main/java/com/uade/beappsint/utils/CommonUtilities.java
+++ src/main/java/com/uade/beappsint/utils/CommonUtilities.java
@@ -4,4 +4,19 @@ public class CommonUtilities {
     public static double formatAmount(double amount) {
         return Math.round(amount * 100.0) / 100.0;
     }
+
+    public static String extractImageFormat(String base64) {
+        String format = null;
+        if (base64 != null && base64.startsWith("data:image/")) {
+            int start = base64.indexOf("/") + 1;
+            int end = base64.indexOf(";");
+            format = base64.substring(start, end);
+        }
+        return format;
+    }
+
+    public static Boolean isValidImageFormat(String format) {
+        if (format == null) return false;
+        return (format.equals("png") || format.equals("jpg") || format.equals("jpeg"));
+    }
 }
